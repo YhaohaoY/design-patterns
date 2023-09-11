65e3c280e883a2d0009ae0fbe0e9bc2698478d92
若科 <yehaohao.yhh@alibaba-inc.com>
2023-09-11 12:44:18 +0800

    insert modlue

---
+++ process-starter/src/main/java/com/example/processstarter/ProcessStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.processstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessStarterApplication.class, args);
+    }
+
+}
---
+++ process-starter/src/test/java/com/example/processstarter/ProcessStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.processstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
